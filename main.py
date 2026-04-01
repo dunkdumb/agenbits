cc2c953fdb14f56ec73370260982bd21064c5440
dunkdumb <enosuppada2005@gmail.com>
2026-04-02 03:40:35 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from agenbits import ImageFile
+
+# Fetch and display image
+img = ImageFile(
+    "https://images.pexels.com/photos/17647410/pexels-photo-17647410/free-photo-of-airplanes-flying-at-an-air-show.jpeg")
+img.show()  # This will open the image using the default viewer
